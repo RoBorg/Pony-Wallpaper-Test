da92e5d4c3b266353580f150072e022142650bc9
RoBorg <roborg@gmail.com>
2017-05-29 16:51:58 +0100

    Initial commit

---
+++ Assets/RoBorg/Pony Wallpaper Test/Scripts/CameraRotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RoBorg.PonyWallpaperTest
+{
+    public class CameraRotate : MonoBehaviour
+    {
+        [SerializeField]
+        private float speed = 1f;
+
+        private void Start()
+        {
+
+        }
+
+        private void Update()
+        {
+            transform.Rotate(new Vector3(0, speed * Time.deltaTime, 0));
+        }
+    }
+}
