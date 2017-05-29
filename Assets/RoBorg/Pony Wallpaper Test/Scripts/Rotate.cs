60e4ce6c584455a58dd211da15f1ec0436c3057a
RoBorg <roborg@gmail.com>
2017-05-29 19:26:01 +0100

    Flapping

---
+++ Assets/RoBorg/Pony Wallpaper Test/Scripts/Rotate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace RoBorg.PonyWallpaperTest
+{
+    public class Rotate : MonoBehaviour
+    {
+        [SerializeField]
+        private float xSpeed = 1f;
+
+        [SerializeField]
+        private float ySpeed = 1f;
+
+        [SerializeField]
+        private float zSpeed = 1f;
+
+        private void Update()
+        {
+            transform.Rotate(new Vector3(xSpeed * Time.deltaTime, ySpeed * Time.deltaTime, zSpeed * Time.deltaTime));
+        }
+    }
+}
