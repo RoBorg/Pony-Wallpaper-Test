728e09021804bba86c6fd5b7df02d452a1023f27
RoBorg <roborg@gmail.com>
2017-05-29 20:22:44 +0100

    Camera mouse

---
+++ Assets/RoBorg/Pony Wallpaper Test/Scripts/RotateCamera.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    [SerializeField]
+    private float ySpeed = 1;
+
+    [SerializeField, Range(1.01f, 2f)]
+    private float yDecay = 2;
+
+    private float currentYVelocity = 0;
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            currentYVelocity = Input.GetAxis("Mouse X") * ySpeed;
+        }
+
+        transform.Rotate(0, currentYVelocity, 0);
+        currentYVelocity /= yDecay;
+    }
+}
